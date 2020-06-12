e5d5a744080c78f89df710345eaa166f16fa509c
loveRandy <543245444@qq.com>
2020-06-12 11:07:05 +0800

    port

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ app.get('/upload/:key', cors(), function(req, res, next){
   })
 })
 
+app.get('/',function(req,res){
+    res.send('randyPaPa')
+})
+
 var port = process.env.PORT || 3000
 app.listen(port, function () {
   console.log('Example app listening on port 3000!')
